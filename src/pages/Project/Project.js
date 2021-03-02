d90efd562ff98ec9d99a600aea996d111e52ed4c
Thoumar <thomas@thoumar.com>
2021-03-02 21:18:01 +0100

    Adding page content

--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,5 +1,7 @@
 import './Project.sass';
 
+import Slider from "react-slick";
+import ReactPlayer from 'react-player'
 import React, { useEffect } from 'react';
 
 import { useLocation } from 'react-router-dom';
@@ -9,15 +11,64 @@ import Menu from './../../components/Menu/Menu';
 
 import { useHistory } from "react-router-dom";
 
+const getComponent = (component, componentKey) => {
+    const multiCarouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+
+    const singleCarouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    switch(component.__component) {
+        case "project-page.carousel":
+            return (
+                <div className="Component__Carousel">
+                    <Slider {...multiCarouselSettings}>
+                        {
+                            component.Pictures.map((pic, key) => <img key={componentKey + key} src={pic.url} alt="carousel item" />)
+                        }
+                    </Slider>
+                </div>
+            )
+        case "project-page.video":
+            return (
+                <div className="Component__Video">
+                    <ReactPlayer url={component.Source} />
+                </div>
+            )
+        case "project-page.single-carousel":
+            return (
+                <div className="Component__Single-carousel">
+                <Slider {...singleCarouselSettings}>
+                    {
+                        component.Pictures.map((pic, key) => <img key={componentKey + key} src={pic.url} alt="carousel item" />)
+                    }
+                </Slider>
+                </div>
+            )
+        default:
+            return null;
+    }
+}
+
 const Project = (data) => {
-    
+
 	const { pathname } = useLocation();
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, [pathname]);
 	
-    const { cover, title, subtitle } = data.project
+    const { cover, title, subtitle, content } = data.project
 
     const history = useHistory();
 
@@ -43,6 +94,13 @@ const Project = (data) => {
                 La nouvelle plateforme de marque a vu le jour lors de campagnes d’affichages  offline et digital ainsi qu’en presse et sur les réseaux sociaux.
                 </p>
             </div>
+
+            {
+                content
+                    ? content.map((component, key) =>  getComponent(component, key))
+                    : null
+                
+            }
 
             <div className="Project__Labels">
                 <div className="Project__Label">AWARD</div>
