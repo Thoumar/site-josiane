21e6e71352542d3962a6b8841f77d744d15c23e3
Thoumar <thomas@thoumar.com>
2021-03-06 12:38:12 +0100

    Adding video player in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player'
 
 import './Header.sass';
 
 import logoWhite from './../../images/logos/logo_white.png';
+// import placeHolderVideo from './../../videos/placeholder_video.mp4';
 import soundOnIcon from './../../images/icons/sound_on.png';
 import soundOffIcon from './../../images/icons/sound_off.png';
 import fullscreenIcon from './../../images/icons/fullscreen.png';
 
-const Header = ({ cover, title, subtitle }) => {
+const Header = ({ cover, title, subtitle, type }) => {
     const [soundState, setSoundState] = useState(false);
+    const [fullScreenState, setFullScreenState] = useState(false);
     const handleSetSoundState = () => {
         setSoundState(!soundState)
     }
@@ -24,16 +27,11 @@ const Header = ({ cover, title, subtitle }) => {
                 }
                 <span>{subtitle}</span>
             </div>
-
             <img className="Header__Button Header__Button--mute" src={soundIconSrc} alt={""} onClick={() => { handleSetSoundState() }} />
-
             <img className="Header__Button Header__Button--fullscreen" src={fullscreenIcon} alt={""} />
-
-            <video className="Header__Background-video" autoPlay muted loop>
-                <source src={cover} type="video/mp4" />
-                Sorry, your browser doesn't support embedded videos.
-            </video>
-
+            <div className="Header__Background-video">
+                <ReactPlayer  playing={true} muted={!soundState} loop url={"https://res.cloudinary.com/thoumar/video/upload/v1615030635/placeholder_video_351fc77517.mp4"} width='100%' height='100%' />
+            </div>
             <div className="Header__Scroller">
                 <span></span>
                 <span>Scroll</span>
