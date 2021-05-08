b3a5386b2bdf1bdc2c625af638fae7c6bf74b064
Thoumar <thomas@thoumar.com>
2021-05-08 13:54:22 +0200

    Edit

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 
 import ReactPlayer from "react-player";
 import ReactSlider from "react-slider";
